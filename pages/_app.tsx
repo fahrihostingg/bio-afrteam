a8adfdb55cf2798f43a52b2fff708b8659f41c85
fahrihostingg <150719442+fahrihostingg@users.noreply.github.com>
2025-02-02 08:09:08 +0800

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,88 @@
+import type { AppProps } from "next/app";
+import { useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
+import "../styles/globals.css";
+
+import { NextSeo } from "next-seo";
+import Head from "next/head";
+import Script from "next/script";
+
+import "@fontsource/jost/400.css";
+import "@fontsource/jost/500.css";
+import "@fontsource/jost/600.css";
+import "@fontsource/jost/700.css";
+import "@fontsource/sen/400.css";
+import "@fontsource/sen/700.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  // Umami Analytics
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://analytics.eu.umami.is/script.js";
+    script.dataset.websiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID; // Mengambil nilai dari variabel lingkungan
+    document.head.appendChild(script);
+  }, []);
+
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+
+      <Analytics />
+
+      <NextSeo
+        title="AFR Team | tetaplah semangat walaupun sering direndahkan"
+        titleTemplate="AFR Tean - Suka bergadang"
+        defaultTitle="AFR Team | Dev Bot"
+        description="Hei! Saya AFR Team , saya suka meneroka semua yang berkaitan dengan teknologi. Selain itu, saya amat meminati dunia teknologi terutamanya bot wa dalam dunia maya!"
+        openGraph={{
+          url: "https://hi-lrmn.is-a.dev/",
+          title: "AFR Team | tetaplah semangat walaupun sering direndahkan",
+          description:
+            "Hei! Saya AFR Team , saya suka meneroka semua yang berkaitan dengan teknologi. Selain itu, saya amat meminati dunia teknologi terutamanya bot wa dalam dunia maya!",
+          images: [
+            {
+              url: "https://hi-lrmn.is-a.dev/og-image.png",
+              alt: "AFR Team  | tetaplah semangat walaupun sering direndahkan",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@romanromannya",
+          site: "@romanromannya",
+          cardType: "summary_large_image",
+        }}
+        additionalMetaTags={[
+          {
+            property: "keywords",
+            content:
+              "AFR Team adalah Dev Bot, Pembangun Web, Mewwme's, Pembangun Web, peminat teknologi, sumber terbuka",
+          },
+        ]}
+      />
+
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
